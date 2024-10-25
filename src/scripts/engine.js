99a11b512ed4640043e6a87d777187e28e6ab507
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2024-10-25 10:54:11 -0300

    create cards dinami dynamically
    Signed-off-by: Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>

---
+++ src/scripts/engine.js
@@ -0,0 +1,11 @@
+const emojis = ["😺","😺","🐸","🐸","🦝","🦝","🦁","🦁","🐯","🐯","🦊","🦊","🐶","🐶","🐼","🐼"];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
